7a68da054914cd662cec947784f4b37654fda9d2
siddiqui-sana <82237417+siddiqui-sana@users.noreply.github.com>
2021-12-07 09:35:27 -0800

    Create Meeting Room II.py

---
+++ https:/www.lintcode.com/problem/919/Meeting Room II.py
@@ -0,0 +1,26 @@
+# Time Complexity: O(nlogn)
+# Space Complexity: O(n)
+
+"""
+Definition of Interval.
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+from heapq import *
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+    def minMeetingRooms(self, intervals):
+        # Write your code here
+        intervals.sort(key=lambda x:x.start)
+        min_heap=[]
+        heappush(min_heap,intervals[0].end)
+        for i in range(1,len(intervals)):
+            if min_heap[0]<=intervals[i].start:
+                heappop(min_heap)
+            heappush(min_heap,intervals[i].end)
+        return len(min_heap)
