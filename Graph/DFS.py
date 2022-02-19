f7cc1f4a070af29c9263bb9a9dee121aaef92cca
siddiqui-sana <82237417+siddiqui-sana@users.noreply.github.com>
2022-02-19 06:12:36 -0800

    Update DFS.py

--- Graph/DFS.py
+++ Graph/DFS.py
@@ -1,3 +1,5 @@
+###Recursive Approach
+
 def dfsOfGraph(V, adj):
     def dfs(node,temp,visited):
         temp.append(node)
@@ -25,3 +27,29 @@ ans=dfsOfGraph(V,adj)
 for i in range(len(ans)):
     print(ans[i])
     
+
+  
+###Stack based approach
+def dfsOfGraph(self, V, adj):
+        visited=[False for i in range(V)]
+        stack=[]
+        res=[]
+        stack.append(0)
+        while(len(stack)!=0):
+            x=stack.pop()
+            if visited[x] is not True:
+                res.append(x)
+                visited[x]=True
+                for i in adj[x][::-1]:
+                    stack.append(i)    
+        return res
+    
+V,E=map(int,input().split())
+adj=[[] for i in range(V+1)]
+for i in range(E):
+    u,v=map(int,input().split())
+    adj[u].append(v)
+    adj[v].append(u)
+ans=dfsOfGraph(V,adj)
+for i in range(len(ans)):
+    print(ans[i])
